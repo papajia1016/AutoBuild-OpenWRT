880c35ed6a3daabba2cff7dd28d2dc72371c4651
XSSS12680 <65147908+XSSS12680@users.noreply.github.com>
2020-05-23 09:01:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#自定插件
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
